910e66337e747981879d1cb0b2ef721485c78bbf
Omar Diab <me@omardiab.com>
2017-10-06 13:29:48 +0900

    fix missing fields impl

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,11 @@ import { ApplicationError } from "../lib/errors";
 /**
  * Helper that throws an error to trigger a 400 response if a
  * field is missing.
- * @param {Array<string>} requiredFields
+ * @param {Array<string>} requiredFields List of required fields
+ * @param {any} body Body of the request
  */
-function assertRequiredFields(requiredFields = []) {
-  const missingFields = requiredFields.filter(field => !!req.body[field]);
+function assertRequiredFields(requiredFields = [], body) {
+  const missingFields = requiredFields.filter(field => !body[field]);
   if (missingFields.length > 0) {
     throw new ApplicationError("Missing required fields", 400, {
       requiredFields,
@@ -27,7 +28,7 @@ export function route(callback, options = {}) {
   return async (req, res, next) => {
     try {
       if (options.requiredFields) {
-        assertRequiredFields(options.requiredFields);
+        assertRequiredFields(options.requiredFields, req.body);
       }
 
       await callback(req, res, next);
