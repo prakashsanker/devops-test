309e1a11998a922a4b5465d59f94ad7e75f244f2
Prakash Sanker <praks5432@gmail.com>
2018-11-30 14:40:53 +0530

    add environment deploys

--- src/router.js
+++ src/router.js
@@ -72,7 +72,7 @@ export default function createRouter() {
     const text = `Hello! We're at commit ${commitRef} which was built at ${
       buildDate
     }`;
-    res.send("FORCE DEPLOY 3");
+    res.send("This should be a deploy only on prod");
     // next(new ApplicationError("Not Found", 404));
   });
 
